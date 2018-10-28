5c109ae37d9c5847e9f8aa7356ade6c7b49383d5
Maks-Chornyi <380960310411@mail.ua>
2018-10-28 16:29:33 +0200

    dev: added new mapping, change autowired by constructor to field

--- src/main/java/portal/event/eventportal/controller/MainController.java
+++ src/main/java/portal/event/eventportal/controller/MainController.java
@@ -5,36 +5,39 @@ import org.springframework.web.bind.annotation.*;
 import portal.event.eventportal.domain.Event;
 import portal.event.eventportal.service.EventService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/event")
+@RequestMapping
 public class MainController {
 
+    @Autowired
     private EventService eventService;
 
-    @Autowired
-    MainController(EventService eventService) {
-        this.eventService = eventService;
+    @GetMapping
+    public void home(HttpServletResponse response) throws IOException {
+        response.sendRedirect("api/event");
     }
 
-    @GetMapping
+    @GetMapping("/api/event")
     public List<Event> getAllEvents() {
         return eventService.getAllEvents();
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/api/event/{id}")
     public Event getEventById(@PathVariable int id) {
         return eventService.getEventById(id);
     }
 
-    @PostMapping("{id}")
+    @PostMapping("/api/event/{id}")
     public Event createEvent(@RequestBody Event event) {
         eventService.createEvent(event);
         return event;
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/event/{id}")
     public Event updateEvent(@PathVariable int id, @RequestBody Event event) {
         return eventService.updateEvent(id,event);
     }
